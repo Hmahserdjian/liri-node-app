a6d5e1211319bc1fe2c02315a821f2cbc9c576e3
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-04-24 09:54:13 -0400

    changes

--- liri.js
+++ liri.js
@@ -1,6 +1,4 @@
-import { runInThisContext } from "vm";
-
-var keys= require("./keys.js");
+var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
@@ -19,44 +17,46 @@ var nodeCall = nodeArgs[2];
 var userInput = "";
 
 for (let i = 2; i < nodeArgs.length; i++) {
-  userInput += nodeArgs[i] + "";
-    
+    userInput += nodeArgs[i] + "";
+
 }
 
 for (var i = 3; i < nodeArgs.length; i++) {
 
-  var address = address + " " + userInput;
+    var address = address + " " + userInput;
 
-   
+
 }
 var spotify1 = function (song) {
-    if (song == undefined){
+    if (song == undefined) {
         song = "the sign";
     }
-     
-(
-    spotify.search(
-         {
-             type: 'track',
-              query: '', 
-              limit: 1 }, 
-function (err, data) {
-    if (err) {
 
-        return console.log('Error occurred: ' + err);
-    }
+    (
+        spotify.search({
+                type: 'track',
+                query: '',
+                limit: 1
+            },
+            function (err, data) {
+                if (err) {
 
-data.tracks.items.forEach(item => {
-    console.log(item.album.release_date);
-    console.log(item.artists[0]);
-});
-    
+                    return console.log('Error occurred: ' + err);
+                }
 
-    
-}));
+                data.tracks.items.forEach(item => {
+                    console.log(item.album.release_date);
+                    console.log(item.artists[0]);
+                });
 
+
+
+            }));
+};
 var tweets1 = function () {
-    { screen_name: 'Skywalkerjabba' };
+    {
+        screen_name: 'SkywalkerJabba'
+    };
 
     client.get('statuses/user_timeline', function (error, tweets, response) {
         if (!error) {
@@ -66,23 +66,23 @@ var tweets1 = function () {
 
 
 }
-}
 
-var thisRun = function(caseData, functionData) {
+
+var thisRun = function (caseData, functionData) {
     switch (caseData) {
-        case "my tweets":
-            tweets1
+        case "my-tweets":
+            tweets1()
             break;
-            case "spotify":
-            if (functionData === undefined) {
+        case "spotify-this-song":
+            if (spotify1 === undefined) {
                 functionData = defaultSong;
             }
             spotify1(functionData);
+            console.log(spotify1)
             break;
     }
-    
+
 
 }
 //this is calling a function called run this so var runThis = your code.
 thisRun(process.argv[2], process.argv[3]);
-
